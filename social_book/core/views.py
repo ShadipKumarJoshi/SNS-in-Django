2a95a3997f991c14b566f077005c4d6e1b992ffb
Shadip Kumar Joshi <220049@softwarica.edu.np>
2025-05-27 12:59:37 +0545

    settings complete

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -29,12 +29,15 @@ def signup(request):
                 user.save()
                 
                 #Log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+                
                 
                 #create a Profile page for new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user= user_model.id)
                 new_profile.save()
-                return redirect('signin')
+                return redirect('settings')
         else:  
             messages.info(request, 'Passwords donnot match!')
             return redirect('signup')
@@ -65,3 +68,32 @@ def signin(request):
 def logout(request):
     auth.logout(request)
     return redirect('signin')
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+    
+    if request.method == 'POST':
+    
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg  # Keep the existing image
+            bio = request.POST['bio']
+            location = request.POST['location']
+            
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        else:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+            
+            user_profile.profileimg = image # Use the uploaded image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+            
+        return redirect('settings')
+        
+    return render(request, 'setting.html', {'user_profile': user_profile})
